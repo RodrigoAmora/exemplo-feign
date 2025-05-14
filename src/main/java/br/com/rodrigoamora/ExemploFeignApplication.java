bc45e6164e3e422e008e59854d0660712047e63b
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-05-14 01:04:21 -0300

    Commit do projeto

---
+++ src/main/java/br/com/rodrigoamora/ExemploFeignApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploFeignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoamora/ExemploFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoamora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploFeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
